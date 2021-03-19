8c7df01f3931c7aa68d91598044956b6be7246d5
Yañez Christian <laithg0279@gmail.com>
2021-03-18 18:12:27 -0700

    18/03/21

--- js/buscar.js
+++ js/buscar.js
@@ -1,6 +1,9 @@
-function cargar() {
+var obj;
+
+function cargarDatos() {
     crearLoad('rcornersEliminar');
-    var obj = [{
+    cargando();
+    obj = [{
         foto: "img/Hector.png",
         nomina: "12345678",
         nombres: "Hector Castro",
@@ -13,10 +16,38 @@ function cargar() {
         carrera: "ISC",
         academia: "ISC"
     }];
+    cargarCarreras();
+    cargarAcademias();
+    cargar(obj);
+    removerLoad();
+}
+
+function cargarCarreras() {
+    var c = document.getElementById('carrera');
+    var carr = ['ISC', 'IGE'];
+    for (var i = 0; i < carr.length; i++) {
+        var op = document.createElement('option');
+        op.value = carr[i];
+        op.innerText = carr[i];
+        c.appendChild(op);
+    }
+}
+
+function cargarAcademias() {
+    var c = document.getElementById('academia');
+    var carr = ['ISC', 'LANI'];
+    for (var i = 0; i < carr.length; i++) {
+        var op = document.createElement('option');
+        op.value = carr[i];
+        op.innerText = carr[i];
+        c.appendChild(op);
+    }
+}
+
+function cargar(obj) {
     for (var i = 0; i < obj.length; i++) {
         construir(obj[i]);
     }
-    removerLoad();
 }
 
 function construir(obj) {
@@ -79,7 +110,7 @@ function construir(obj) {
     del.type = "button";
     del.value = "Modificar";
     del.addEventListener("click", function() {
-        cambiar('modificar')
+        modificar(value[0]);
     }, false);
     trD.appendChild(del);
     var tdD = document.createElement("td");
@@ -90,11 +121,104 @@ function construir(obj) {
     del.type = "button";
     del.value = "Visualizar";
     del.addEventListener("click", function() {
-        cambiar('visualizar')
+        vis(value[0]);
     }, false);
     trD.appendChild(del);
     tabla.appendChild(trD);
     tdI.appendChild(tabla);
     tr1.appendChild(tdI);
     t.appendChild(tr1);
+}
+
+
+function buscar() {
+    recrear();
+    var aux = [];
+    var nombre = document.getElementById('SearchName').value;
+    var nomina = document.getElementById('sMat').value;
+    var carrera = document.getElementById('carrera').value;
+    var academia = document.getElementById('academia').value;
+    aux = porNombre(nombre, obj, aux);
+    aux = porNomina(nomina, obj, aux);
+    aux = porCarrera(carrera, obj, aux);
+    aux = porAcademia(academia, obj, aux);
+    if (nombre.length < 1 && nomina.length < 1 && carrera.length < 1 && academia.length < 1)
+        aux = obj;
+    else
+        aux = filtro(aux);
+    cargar(aux);
+}
+
+function filtro(obj) {
+    var aux = [];
+    for (var i = 0; i < obj.length; i++) {
+        var f = true;
+        for (var k = 0; k < aux.length && f; k++) {
+            if (obj[i]['nomina'].includes(aux[k]['nomina'])) {
+                f = false;
+            }
+        }
+        if (f)
+            aux.push(obj[i]);
+    }
+    return aux;
+}
+
+function porNombre(valor, obj, aux) {
+    if (valor.length > 0) {
+        for (var i = 0; i < obj.length; i++) {
+            if (obj[i]['nombres'].includes(valor)) {
+                aux.push(obj[i]);
+            }
+        }
+    }
+    return aux;
+}
+
+function porNomina(valor, obj, aux) {
+    if (valor.length > 0) {
+        for (var i = 0; i < obj.length; i++) {
+            if (obj[i]['nomina'].includes(valor)) {
+                aux.push(obj[i]);
+            }
+        }
+    }
+    return aux;
+}
+
+function porCarrera(valor, obj, aux) {
+    if (valor.length > 0) {
+        for (var i = 0; i < obj.length; i++) {
+            if (obj[i]['carrera'].includes(valor)) {
+                aux.push(obj[i]);
+            }
+        }
+    }
+    return aux;
+}
+
+function porAcademia(valor, obj, aux) {
+    if (valor.length > 0) {
+        for (var i = 0; i < obj.length; i++) {
+            if (obj[i]['academia'].includes(valor)) {
+                aux.push(obj[i]);
+            }
+        }
+    }
+    return aux;
+}
+
+function recrear() {
+    document.getElementById("tabla").remove();
+    var page = document.getElementById("page");
+    var t = document.createElement("table");
+    t.setAttribute("id", "tabla");
+    t.setAttribute("cellspacing", "3");
+    t.setAttribute("cellpadding", "3");
+    t.setAttribute("class", "tablaElimiar");
+    page.appendChild(t);
+}
+
+function limpiar() {
+    cambiar('eliminar');
 }
--- js/menu.js
+++ js/menu.js
@@ -16,6 +16,16 @@ function cerrarSesion() {
     window.location.href = "index.html";
 }
 
-function modificar() {
-    window.location.href = "modificar.html?id=" + id + "&mod=" + id;
+function modificar(mod = -1) {
+    if (mod == -1)
+        window.location.href = "modificar.html?id=" + id + "&mod=" + id;
+    else
+        window.location.href = "modificar.html?id=" + id + "&mod=" + mod;
+}
+
+function vis(vis = -1) {
+    if (vis == -1)
+        window.location.href = "visualizar.html?id=" + id + "&vis=" + id;
+    else
+        window.location.href = "visualizar.html?id=" + id + "&vis=" + vis;
 }
