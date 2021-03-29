6af6e862535543282dabbf42018a3c6833c1ec44
Yañez Christian <laithg0279@gmail.com>
2021-03-28 18:25:35 -0700

    28/03/21

--- js/buscar.js
+++ js/buscar.js
@@ -51,81 +51,145 @@ function cargar(obj) {
 }
 
 function construir(obj) {
-    var t = document.getElementById("tabla");
-    //tr Principal
-    var tr1 = document.createElement("tr");
-    tr1.setAttribute("class", "trContentElimiar");
-    //Td de la foto
-    var tdF = document.createElement("td");
-    tdF.setAttribute("class", "tdImgEliminar");
-    var img = document.createElement("img");
-    img.setAttribute("class", "imgEliminar");
+    var t = document.getElementById('tabla');
+    var tbody = document.getElementById('bodytabla');
+    var tr1 = document.createElement('tr');
+    tr1.setAttribute('class', 'trContentElimiar');
+    var td1 = document.createElement('td');
+    td1.setAttribute('class', 'tdImgEliminar');
+    var img = document.createElement('img');
+    img.setAttribute('class', 'imgEliminar');
     img.src = obj['foto'];
-    tdF.appendChild(img);
-    tr1.appendChild(tdF);
-    //Td de la info
-    var tdI = document.createElement("td");
-    tdI.setAttribute("class", "tdContenidoEliminar");
-    //contenedor de la info
-    var tabla = document.createElement("table");
-    var trI = document.createElement("tr");
-    var tdI2 = document.createElement("td");
-    //tabla final
-    var tablaF = document.createElement("table");
-    tablaF.setAttribute("class", "cuerpoEliminar");
-    tablaF.setAttribute("cellspacing", "3");
-    tablaF.setAttribute("cellpadding", "3");
+    td1.appendChild(img);
+    tr1.appendChild(td1);
+    var td2 = document.createElement('td');
+    td2.setAttribute('class', 'tdContenidoEliminar');
+    var tabla2 = document.createElement('table');
+    var tb = document.createElement('tbody');
+    var tr3 = document.createElement('tr');
+    var td31 = document.createElement('td');
+    var tablatd31 = document.createElement('table');
+    tablatd31.setAttribute('class', 'cuerpoEliminar');
+    tablatd31.setAttribute('cellspacing', '3');
+    tablatd31.setAttribute('cellpadding', '3');
+    var tbody31 = document.createElement('tbody');
     var key = ["Nómina:", "Nombre(s):", "Carrera:", "Academia:"];
     var value = [obj["nomina"], obj["nombres"], obj["carrera"], obj["academia"]];
     for (var i = 0; i < key.length; i++) {
-        var trNomina = document.createElement("tr");
-        var tdLabel = document.createElement("td");
-        var labelNom = document.createElement("label");
-        labelNom.setAttribute("class", "contenidoEliminar");
-        labelNom.textContent = key[i];
-        tdLabel.appendChild(labelNom);
+        var trtbody31 = document.createElement('tr');
+        var tdtbody31 = document.createElement('td');
+        var label = document.createElement('label');
+        label.setAttribute('class', 'contenidoEliminar');
+        label.innerText = key[i];
+        tdtbody31.appendChild(label);
+        trtbody31.appendChild(tdtbody31);
 
-        var tdInp = document.createElement("td");
-        var inp = document.createElement("input");
+        var td2tbody31 = document.createElement('td');
+        var inp = document.createElement('input');
         inp.disabled = true;
         inp.value = value[i];
-        tdInp.appendChild(inp);
-
-        trNomina.appendChild(tdLabel);
-        trNomina.appendChild(tdInp);
-        tablaF.appendChild(trNomina);
+        td2tbody31.appendChild(inp);
+        trtbody31.appendChild(td2tbody31);
+        tbody31.appendChild(trtbody31);
     }
-    tdI2.appendChild(tablaF);
-    trI.appendChild(tdI2);
-    tabla.appendChild(trI);
-    //elimiar
-    var trD = document.createElement("tr");
-    var tdD = document.createElement("td");
-    tdD.setAttribute("class", "tdbtnEliminar");
-    //boton mod
-    var del = document.createElement("input");
-    del.setAttribute("class", "button button2Eliminar");
-    del.type = "button";
-    del.value = "Modificar";
-    del.addEventListener("click", function() {
-        modificar(value[0]);
-    }, false);
-    trD.appendChild(del);
-    var tdD = document.createElement("td");
-    tdD.setAttribute("class", "tdbtnEliminar");
-    //boton mod
-    var del = document.createElement("input");
-    del.setAttribute("class", "button button2Eliminar");
-    del.type = "button";
-    del.value = "Visualizar";
-    del.addEventListener("click", function() {
-        vis(value[0]);
-    }, false);
-    trD.appendChild(del);
-    tabla.appendChild(trD);
-    tdI.appendChild(tabla);
-    tr1.appendChild(tdI);
-    t.appendChild(tr1);
+    tablatd31.appendChild(tbody31);
+    td31.appendChild(tablatd31);
+    tr3.appendChild(td31);
+    var td32 = document.createElement('td');
+    var tablatd32 = document.createElement('table');
+    tablatd32.setAttribute('class', 'cuerpoEliminar');
+    tablatd32.setAttribute('cellspacing', '3');
+    tablatd32.setAttribute('cellpadding', '3');
+    tablatd32.setAttribute('name', 'ok');
+    var tbody32 = document.createElement('tbody');
+    var permisosAdmin = ['Plan de trabajo (crear, modificar)', 'Acciones al personal(alta, baja, modificar)',
+        'Acciones a academias (alta, baja, modificar)', 'Acciones a carrera (alta, modificar)'
+    ];
+    var trPermisosTitlulo = document.createElement('tr');
+    var th = document.createElement('th');
+    th.innerText = 'Permisos de Admin:';
+    trPermisosTitlulo.appendChild(th);
+    tbody32.appendChild(trPermisosTitlulo);
+    for (var i = 0; i < permisosAdmin.length; i++) {
+        var trPermisos = document.createElement('tr');
+        var tdPermisos = document.createElement('td');
+        var inpPermisos = document.createElement('input');
+        inpPermisos.setAttribute('type', 'checkbox');
+        var labelPer = document.createElement('label');
+        labelPer.setAttribute('class', 'contenidoEliminar');
+        labelPer.innerText = permisosAdmin[i];
+        tdPermisos.appendChild(inpPermisos);
+        tdPermisos.appendChild(labelPer);
+        trPermisos.appendChild(tdPermisos);
+        tbody32.appendChild(trPermisos);
+    }
+    tablatd32.appendChild(tbody32);
+    td32.appendChild(tablatd32);
+    tr3.appendChild(td32);
+    var td33 = document.createElement('td');
+    var tablatd33 = document.createElement('table');
+    tablatd33.setAttribute('class', 'cuerpoEliminar');
+    tablatd33.setAttribute('cellspacing', '3');
+    tablatd33.setAttribute('cellpadding', '3');
+    var tbody33 = document.createElement('tbody');
+    var trPermisosTitlulo = document.createElement('tr');
+    var th = document.createElement('th');
+    th.innerText = 'Permisos generales:';
+    trPermisosTitlulo.appendChild(th);
+    tbody33.appendChild(trPermisosTitlulo);
+    var permisosGen = ['Actas (crear y modificar)', 'Evaluar Profesor (crear y modificar)',
+        'Evaluar Presidente(crear y modificar)'
+    ];
+    for (var i = 0; i < permisosGen.length; i++) {
+        var trPermisos = document.createElement('tr');
+        var tdPermisos = document.createElement('td');
+        var inpPermisos = document.createElement('input');
+        inpPermisos.setAttribute('type', 'checkbox');
+        var labelPer = document.createElement('label');
+        labelPer.setAttribute('class', 'contenidoEliminar');
+        labelPer.innerText = permisosGen[i];
+        tdPermisos.appendChild(inpPermisos);
+        tdPermisos.appendChild(labelPer);
+        trPermisos.appendChild(tdPermisos);
+        tbody33.appendChild(trPermisos);
+    }
+    var trBtnPermisos = document.createElement('tr');
+    var tdBtnPermisos = document.createElement('td');
+    var AplicarPermisos = document.createElement('input');
+    AplicarPermisos.setAttribute('class', 'button buttonAplicarP');
+    AplicarPermisos.type = 'button';
+    AplicarPermisos.value = 'Aplicar Permisos';
+    AplicarPermisos.addEventListener('click', function() {
+        alert(obj["nomina"]);
+    }, false)
+    tdBtnPermisos.appendChild(AplicarPermisos);
+    trBtnPermisos.appendChild(tdBtnPermisos);
+    tbody33.appendChild(trBtnPermisos);
+    tablatd33.appendChild(tbody33);
+    td33.appendChild(tablatd33);
+    tr3.appendChild(td33);
+    tb.appendChild(tr3);
+
+    var tr2 = document.createElement('tr');
+    var td22 = document.createElement('td');
+    var Mod = document.createElement('input')
+    Mod.setAttribute('class', 'button button2Eliminar');
+    Mod.type = 'button';
+    Mod.value = 'Modificar';
+    var Vis = document.createElement('input')
+    Vis.setAttribute('class', 'button button2Eliminar');
+    Vis.type = 'button';
+    Vis.value = 'Visualizar';
+
+    td22.appendChild(Mod);
+    td22.appendChild(Vis);
+    tr2.appendChild(td22);
+    tb.appendChild(tr2);
+    tabla2.appendChild(tb);
+    td2.appendChild(tabla2);
+    tr1.appendChild(td2);
+    tbody.appendChild(tr1);
+    t.appendChild(tbody);
 }
 
 
@@ -210,10 +274,13 @@ function recrear() {
     document.getElementById("tabla").remove();
     var page = document.getElementById("page");
     var t = document.createElement("table");
+    var tbody = document.createElement('tbody');
+    tbody.setAttribute('id', 'bodytabla');
     t.setAttribute("id", "tabla");
     t.setAttribute("cellspacing", "3");
     t.setAttribute("cellpadding", "3");
     t.setAttribute("class", "tablaElimiar");
+    t.appendChild(tbody);
     page.appendChild(t);
 }
 
